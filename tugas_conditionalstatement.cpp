7bb0706eaa3cad94ad271bfc39bbfacacb2cba02
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-03-09 20:36:24 +0700

    menambahkan cout

--- tugas_conditionalstatement.cpp
+++ tugas_conditionalstatement.cpp
@@ -5,3 +5,20 @@ using namespace std;
 float Hitung_BMI(float berat, float tinggi){
     return berat/(tinggi*tinggi);
 }
+
+//status BMI
+string Status_BMI(float BMI){
+    if(BMI < 18.5)
+        return "berat badan kurang";
+    else if(BMI < 25)
+        return "berat badan normal";
+    else if(BMI < 30)
+        return "berat badan kelebihan";
+    else
+        return "obesitas";
+}
+
+//Input Data
+void Input_Data(float & berat, float & tinggi){
+    cout << "masukan berat badan (kg) : ";
+}
